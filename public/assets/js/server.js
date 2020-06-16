c055082a0d4f19b9b2ce6caf3f0a81a3b9485e94
yulia tikhonova <yuliatikhonova@outlook.com>
2020-06-15 23:06:00 -0700

    added the path to index and notes.html in server.js

--- public/assets/js/server.js
+++ public/assets/js/server.js
@@ -5,3 +5,14 @@ const express = require('express');
 const app = express();
 
 const PORT = process.env.PORT || 8080;
+
+
+app.get("/", function(req, res) {
+    res.sendFile(path.join(__dirname + "/index.html"));
+  });
+
+app.get("/notes", function(req, res) {
+    res.sendFile(path.join(__dirname + "/notes.html"));
+  });
+
+  app.listen(port, () => console.log(`Its listening to port ${port}!`))
